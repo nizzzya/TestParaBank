d4b04aa94fef0c6072d0f50e44ee481f862d0e51
nizzzya <vladislav.shkarlat@gmail.com>
2022-11-15 12:21:53 +0200

    Marge branch

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,6 +3,9 @@ package org.example;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println(
+                String.format("The current shell is: %s.", System.getenv("SHELL"))
+        );
 
     }
 }
